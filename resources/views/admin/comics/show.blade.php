33a1f53f23c0aeebfcf1906adf417573d2db0923
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-05-29 17:58:37 +0200

    change style show.blade

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -4,10 +4,13 @@
     <div class="container py-5">
         <h1 class="mb-3">Admin Show Once</h1>
 
+        <div class="d-flex flex-row">
 
-        <div class="card" style="width: 18rem;">
-            <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->series }}">
-            <div class="card-body d-flex flex-column gap-4">
+            <div>
+                <img src="{{ $comic->thumb }}" class="card-img-top" style="width: 18rem;" alt="{{ $comic->series }}">
+            </div>
+
+            <div class="d-flex flex-column gap-2 h-100 ms-4">
                 <h5 class="card-title">{{ $comic->series }}</h5>
                 <span><strong>Title: </strong><br>{{ $comic->title }}</span>
                 <span><strong>Type: </strong><br>{{ $comic->type }}</span>
@@ -15,6 +18,8 @@
                 <span><strong>Date: </strong><br>{{ $comic->sale_date }}</span>
                 <p class="card-text"><strong>Description: </strong><br>{{ $comic->description }}</p>
             </div>
+
         </div>
+
     </div>
 @endsection
